066f8772ea428ed45e7f2108e6c4861e479ef9b3
Ankit Sharma <ankitbond7007@gmail.com>
2023-07-04 16:36:31 +0000

    lec12

--- Q1(word search), Q2(power(x,n)), Q3(power set)/powe(n,p).cpp
+++ Q1(word search), Q2(power(x,n)), Q3(power set)/powe(n,p).cpp
@@ -5,10 +5,10 @@ using namespace std;
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0)return 1;
-        if(n==1)return x;
-        if(n>0){
-        if(n%2==0)return myPow(x*x,n/2);
+        if(n == 0)return 1;
+        if(n == 1)return x;
+        if(n > 0){
+        if(n%2 ==0 )return myPow(x*x,n/2);
         else return x*myPow(x*x,n/2);
         }
         else{
