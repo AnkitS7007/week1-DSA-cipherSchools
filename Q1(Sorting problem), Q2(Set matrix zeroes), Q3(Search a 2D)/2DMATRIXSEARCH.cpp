ffdcda542e41ab1966150f5ca65e43a2119e3390
Ankit Sharma <ankitbond7007@gmail.com>
2023-07-04 16:33:33 +0000

    lec8

--- Q1(Sorting problem), Q2(Set matrix zeroes), Q3(Search a 2D)/2DMATRIXSEARCH.cpp
+++ Q1(Sorting problem), Q2(Set matrix zeroes), Q3(Search a 2D)/2DMATRIXSEARCH.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool searchMatrix(vector<vector<int>> &matrix, int target){
-    int row = matrix.size()-1;
+    int row = matrix.size() - 1;
     if(row<0){
         return false;
     }
